42a24f2ac649be3a84e51d4e23548e4ef2922e63
evil-A <57819409+evil-A@users.noreply.github.com>
2021-11-13 09:27:08 +0100

    Add files via upload

---
+++ cpp02/ex01/Fixed.cpp
@@ -0,0 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: evila-ro <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/13 08:53:55 by evila-ro          #+#    #+#             */
+/*   Updated: 2021/11/13 09:26:37 by evila-ro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "Fixed.hpp"
+
+Fixed::Fixed(void)
+{
+	std::cout << "Default constructor called" << std::endl;
+	return ;
+}
+
+Fixed::~Fixed(void)
+{
+	std::cout << "Destructor called" << std::endl;
+	return ;
+}
+
+Fixed::Fixed(int const i)
+{
+	std::cout << "Int constructor called" << std::endl;
+	return ;
+}
+
+Fixed::Fixed(float const f)
+{
+	std::cout << "Float constructor called" << std::endl;
+	return ;
+}
+
+Fixed::Fixed(Fixed const &cop)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	*this = cop;
+	return ;
+}
+
+Fixed	&Fixed::operator = (Fixed const &ass)
+{
+	 std::cout << "Assignation operator called" << std:endl;
+	 if (this != &ass)
+		 this->_point = ass.getRawBits();
+	 return (*this);
+}
+
+void	Fixed::setRawBits(int const raw)
+{
+	this->_point = raw;
+}
+
+int		Fixed::getRawbits(void)const
+{
+	return (this->_point);
+}
+
+int		Fixed::toInt(void)const
+{
+
+	//return (static_cast<int>(this->_point));//c++11
+}
+
+float	Fixed::toFloat(void)const
+{
+
+	//return (static_cast<float>(this->_point));//c++11
+}
+
+//An overload to the « operator that inserts a floating point representation of the fixed point value into the parameter output stream.
---
+++ cpp02/ex01/Fixed.hpp
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: evila-ro <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/13 08:22:11 by evila-ro          #+#    #+#             */
+/*   Updated: 2021/11/13 09:26:39 by evila-ro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+class Fixed
+{
+
+	private:
+
+		int					_point;//converts it to the correspondant fixed(8) point value.
+		static int const	_frac = 8;
+
+	public:
+
+	Fixed(void);
+	~Fixed(void);
+	Fixed(int const i);
+	Fixed(float const f);
+	Fixed(Fixed const &cop);
+	Fixed &operator = (Fixed const &ass);
+
+	void	setRawBits(int const raw);
+	int		getRawBits(void)const;
+	float	toFloat(void)const;
+	int		toInt(void)const;
+
+};
+//An overload to the « operator that inserts a floating point representation of the fixed point value into the parameter output stream.
+
+#endif
