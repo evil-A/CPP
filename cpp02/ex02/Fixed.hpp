ea04677407fc0732d31b90fe76cd2018a317af43
evil-A <57819409+evil-A@users.noreply.github.com>
2021-11-16 09:00:16 +0100

    Add files via upload

--- cpp02/ex02/Fixed.hpp
+++ cpp02/ex02/Fixed.hpp
@@ -6,8 +6,57 @@
 /*   By: evila-ro <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/14 03:38:31 by evila-ro          #+#    #+#             */
-/*   Updated: 2021/11/14 03:38:33 by evila-ro         ###   ########.fr       */
+/*   Updated: 2021/11/16 08:34:36 by evila-ro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef  FIXED_H
+# define FIXED_H
 
+#include <fstream>
+
+class Fixed
+{
+	private:
+
+		int					_point;
+		static int const	_frac = 8;
+
+	public:
+
+		Fixed(void);
+		~Fixed(void);
+		Fixed(int const i);
+		Fixed(float const f);
+		Fixed(Fixed const &src);
+
+		Fixed	&operator=(Fixed const &ass);
+		Fixed	operator+(Fixed const &ass)const;
+		Fixed	operator-(Fixed const &ass)const;
+		Fixed	operator*(Fixed const &ass)const;
+		Fixed	operator/(Fixed const &ass)const;
+		bool	operator>(Fixed const &ass)const;
+		bool	operator<(Fixed const &ass)const;
+		bool	operator>=(Fixed const &ass)const;
+		bool	operator<=(Fixed const &ass)const;
+		bool	operator==(Fixed const &ass)const;
+		bool	operator!=(Fixed const &ass)const;
+		Fixed	&operator++(void);//prefix
+		Fixed	operator++(int);//postfix
+		Fixed	&operator--(void);//prefix
+		Fixed	operator--(int);//postfix
+		static Fixed const	&min(Fixed const &a, Fixed const &b);
+		static Fixed		&min(Fixed &a, Fixed &b);
+		static Fixed const	&max(Fixed const &a, Fixed const &b);
+		static Fixed		&max(Fixed &a, Fixed &b);
+
+		void	setRawBits(int const raw);
+		int		getRawBits(void)const;
+		float	toFloat(void)const;
+		int		toInt(void)const;
+
+};
+
+std::ostream	&operator<<(std::ostream &output, Fixed const &fix);
+
+#endif
--- cpp02/ex02/main.cpp
+++ cpp02/ex02/main.cpp
@@ -6,8 +6,51 @@
 /*   By: evila-ro <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/14 03:38:55 by evila-ro          #+#    #+#             */
-/*   Updated: 2021/11/14 03:38:58 by evila-ro         ###   ########.fr       */
+/*   Updated: 2021/11/16 08:45:13 by evila-ro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <iomanip>
+#include "Fixed.hpp"
 
+int main(void)
+{
+	Fixed			a;
+	Fixed const		b(Fixed(5.05f) + Fixed(2));
+	Fixed const		c(Fixed(5.05f) - Fixed(2));
+	Fixed const		d(Fixed(5.05f) * Fixed(2));
+	Fixed const		e(Fixed(5.05f) / Fixed(2));
+	Fixed			f(666);
+	
+
+	std::cout << "a : " << a << std::endl;
+	std::cout << "b : " << b << std::endl;
+	std::cout << "c : " << c << std::endl;
+	std::cout << "d : " << d << std::endl;
+	std::cout << "e : " << e << std::endl;
+	if (b > c)
+		std::cout << "Higher" << std::endl;
+	if (c < b)
+		std::cout << "Lower" << std::endl;
+	if (b >= c)
+		std::cout << "Higher or equal" << std::endl;
+	if (c <= b)
+		std::cout << "Lower or equal" << std::endl;
+	if (c == c)
+		std::cout << "Equal" << std::endl;
+	if (c != b)
+		std::cout << "Not equal" << std::endl;
+
+	std::cout << "Min " << b << " " << c << " " << (Fixed::min(b, c)) << std::endl;
+	std::cout << "Max " << b << " " << c << " " << (Fixed::max(b, c)) << std::endl;
+	std::cout << "Const min " << b << " " << c << " " << (Fixed::min(b, c)) << std::endl;
+	std::cout << "Const max " << b << " " << c << " " << (Fixed::max(b, c)) << std::endl;
+
+	std::cout << a << "++ " << a++ << a << std::endl;
+	std::cout << a << "-- " << a-- << a << std::endl;
+	std::cout << a << "-- " << a-- << a << std::endl;
+	std::cout << f << "++ " << f++ << f << std::endl;
+	std::cout << f << "-- " << f-- << f << std::endl;
+	return (0);
+}
